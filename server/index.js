eddb00353462dec945cc71fea7eea113a6df81aa
temal07 <temiralimov38@gmail.com>
2024-04-20 16:05:21 -0400

    Connect to MongoDB and migrate from nodemon to node --watch (finally!)

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose
+    .connect(
+        process.env.MONGO_URI
+    )
+    .then(() => {
+        console.log('MongoDB is connected to the app...');
+    })
 
 const app = express();
-
-app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is up and listening on 3000!');
