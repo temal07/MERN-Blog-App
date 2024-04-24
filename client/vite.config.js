a6a02ea185287f5f3e6008c02d83527a978fa4f6
temal07 <temiralimov38@gmail.com>
2024-04-23 22:03:50 -0400

    add functionality to the sign-up page

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    // set up a proxy for making localhost:3000 
+    // and localhost:5173 communicate with each other
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      }
+    },
+  },
   plugins: [react()],
 })
